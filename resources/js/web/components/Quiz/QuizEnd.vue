9fadbab68ffaf4b0bf8da59fff5df03037e3f06c
Tomas Pavlik <info@tomaspavlik.cz>
2025-01-10 08:45:46 +0100

    Fix final evaluation

--- resources/js/web/components/Quiz/QuizEnd.vue
+++ resources/js/web/components/Quiz/QuizEnd.vue
@@ -10,26 +10,26 @@
             <progress-bar class="mb-5"/>
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-x-2">
-                <div v-if="summary.evaluation.right.length"
+                <div v-if="summary.statistics.correctAnswers"
                      class="shadow-green-700 lg:mb-8 border-green-700 border-4 p-3 rounded-lg mb-5">
                     <div class="text-center font-semibold mb-8">
                         Správných odpovědí: {{ summary.statistics.correctAnswers }}<br/>
                         Úspěšnost: {{ summary.statistics.percentageCorrectAnswers }}%
                     </div>
 
-                    <ul class="list-disc lg:mx-10 ml-5">
+                    <ul v-if="summary.evaluation.right.length" class="list-disc lg:mx-10 ml-5">
                         <li v-for="data in summary.evaluation.right" class="mb-5" v-html="data"></li>
                     </ul>
                 </div>
 
-                <div v-if="summary.evaluation.wrong.length"
+                <div v-if="summary.statistics.incorrectAnswers"
                      class="shadow-red-700 lg:mb-8 border-red-700 border-4 p-3 rounded-lg">
                     <div class="text-center font-semibold mb-8">
                         Chybných odpovědí: {{ summary.statistics.incorrectAnswers }}<br/>
                         Chybovost: {{ summary.statistics.percentageIncorrectAnswers }}%
                     </div>
 
-                    <ul class="list-disc lg:mx-10 ml-5">
+                    <ul v-if="summary.evaluation.wrong.length" class="list-disc lg:mx-10 ml-5">
                         <li v-for="data in summary.evaluation.wrong" class="mb-5" v-html="data"></li>
                     </ul>
                 </div>
@@ -42,8 +42,6 @@
 import {getRespondentSummary} from '../../services/QuizAPI.js';
 import {onMounted, ref} from 'vue';
 import Loading from '../Loading.vue';
-import IconHandThumbUp from '../Icons/IconHandThumbUp.vue';
-import IconHandRaised from '../Icons/IconHandRaised.vue';
 import ProgressBar from "./ProgressBar.vue";
 
 const summary = ref([]);
