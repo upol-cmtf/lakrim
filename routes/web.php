08910fa7a5095ba667bebbaf7c879e304715f5e1
Tomas Pavlik <info@tomaspavlik.cz>
2025-05-16 21:46:06 +0200

    CU-8698mb2x6 main screen

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,10 @@ Route::prefix('kviz')->group(function () {
     Route::get('{quizEvent:hash?}', [Web\Quiz\QuizRunController::class, 'run'])
         ->name('web.quiz.run');
 });
+
+### Version 2
+Route::prefix('v2')->group(function () {
+    Route::get('', fn() => view('web.quiz-grid.index'))
+        ->name('web.quiz-grid.index');
+});
+
