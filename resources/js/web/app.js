b4f4fa32e6222f2890080ba4e92f34145a631c1c
Tomáš Pavlík <info@tomaspavlik.cz>
2026-05-04 21:30:27 +0200

    v3 new graphic

--- resources/js/web/app.js
+++ resources/js/web/app.js
@@ -2,8 +2,11 @@ import {createApp} from 'vue';
 import {createPinia} from 'pinia';
 import mitt from 'mitt';
 
+import.meta.glob(['../../images/**']);
+
 import ButtonBlue from './components/ButtonBlue.vue';
 import IconArrowRight from './components/Icons/IconArrowRight.vue';
+import IslandScene from './components/IslandGame/IslandScene.vue';
 import QuizForm from './components/Quiz/QuizForm.vue';
 import QuestionsTiles from "./components/QuizGrid/QuestionsTiles.vue";
 
@@ -17,6 +20,7 @@ app.provide('EventBus', EventBus);
 
 app.component('ButtonBlue', ButtonBlue)
 	.component('IconArrowRight', IconArrowRight)
+	.component('IslandScene', IslandScene)
 	.component('QuizForm', QuizForm)
 	.component('QuestionsTiles', QuestionsTiles);
 
